a9fcadba3046100c382c3bd87e1dc5d2dd566bec
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-01 03:46:16 +0000

    Task 4: done / task 3 is still functioning / code atm is efficient and readable / good documentation /  Feb18-11:46

--- models/base_model.py
+++ models/base_model.py
@@ -19,16 +19,18 @@ class BaseModel:
         str: name of the user
         number: id number
         """
-        if len(kwargs) == 0:
-            self.id = str(uuid.uuid4())
-            self.created_at = _iso8601_format(datetime.now())
-            self.updated_at = _iso8601_format(datetime.now())
-        else:
+
+        if kwargs:
             for key, value in kwargs.items():
                 self.__dict__[key] = value
-            del self.__dict__['__class__']
-            self.__dict__["created_at"] = datetime.strptime(
-                self.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """
@@ -43,7 +45,7 @@ class BaseModel:
         Updates the instance to the current
         day and time
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     '''Public instance method'''
 
@@ -54,4 +56,6 @@ class BaseModel:
         """
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
