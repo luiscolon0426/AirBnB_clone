00af6787ce58f8929639940ffcf7d3fefc630dcf
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-03 01:31:23 +0000

    create method is functioning, now we working with the show method of task 7

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ contains the entry point of the command interpreter
 """
 
 import cmd
+from models.base_model import BaseModel
+from models.__init__ import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,19 +13,79 @@ class HBNBCommand(cmd.Cmd):
     HBnB console
     """
 
+    allowed_classes = {"BaseModel"}
+
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """ exit the programm """
+        """
+        exit the programm
+        """
         return True
 
     def do_EOF(self, arg):
-        """ exit the programm """
+        """
+        exit the programm
+        """
         return True
 
     def emptyLine(self):
-        """ empty line """
+        """
+        empty line
+        """
         return False
+
+    def do_create(self, arg):
+        """
+        creates instance
+        """
+        if len(arg) == 0:
+            print("** class name missing **")
+
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
+
+        elif arg:
+            create_dic = {"BaseModel": BaseModel}
+            new = create_dic[arg]()
+            print(new.id)
+            storage.new(new)
+            storage.save()
+
+    def do_show(self, arg):
+        new = arg.partition(" ")
+        cl_name = new[0]
+        cl_id = new[2]
+
+        if not cl_name:
+            print("** class name missing **")
+            return
+
+        if cl_name not in HBNBCommand.allowed_classes:
+            print("** class doesn't exist **")
+            return
+
+        if not cl_id:
+            print("** instance id missing **")
+            return
+
+        key = cl_name + "." + cl_id
+        try:
+            print(storage._FileStorage__objects[key])
+        except KeyError:
+            print("** no instance found **")
+
+        # elif arg:
+        #     show_dic = {"BaseModel": BaseModel}
+        #     instance = show_dic[arg]()
+
+        #     line = self.parseline(arg)[0]
+        #     if not isinstance(line[1], BaseModel()):
+        #         print("** no instance found **")
+
+    def do_destroy(self, arg)
+    new = arg.partition(" ")
+    
 
 
 if __name__ == '__main__':
